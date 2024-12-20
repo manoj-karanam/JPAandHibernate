580d06b739d5f8e165c806e90b4cf920502c6cda
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-12-20 12:58:58 -0500

    added new tbale/entity called AccessCard

---
+++ jpa-starter/src/main/java/io/javabrains/AccessCard.java
@@ -0,0 +1,53 @@
+package io.javabrains;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class AccessCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private Date issueDate;
+
+    private boolean isActive;
+
+    private String firmwareVersion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(Date issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getFirmwareVersion() {
+        return firmwareVersion;
+    }
+
+    public void setFirmwareVersion(String firmwareVersion) {
+        this.firmwareVersion = firmwareVersion;
+    }
+}
--- jpa-starter/src/main/java/io/javabrains/JpaStarterMain.java
+++ jpa-starter/src/main/java/io/javabrains/JpaStarterMain.java
@@ -9,52 +9,48 @@ import java.util.Date;
 public class JpaStarterMain {
     public static void main(String[] args) {
 
-        //updating a field
+        Employee employee = new Employee();
+//        employee.setId(1);
+        employee.setName("Foo Bar");
+        employee.setSsn("123");
+        employee.setDob(new Date());
+        employee.setAge(20);
+        employee.setType(EmployeeType.CONTRACTOR);
+
+        Employee employee2 = new Employee();
+//        employee2.setId(2);
+        employee2.setName("Bar bazzzz");
+        employee2.setSsn("456");
+        employee2.setDob(new Date());
+        employee2.setAge(30);
+        employee2.setType(EmployeeType.FULL_TIME);
+
+        AccessCard card1 = new AccessCard();
+        AccessCard card2 = new AccessCard();
+        card1.setIssueDate(new Date());
+        card1.setActive(true);
+        card1.setFirmwareVersion("1.0.0");
+
+        card2.setIssueDate(new Date());
+        card2.setActive(false);
+        card2.setFirmwareVersion("1.2.0");
+
+
+
+
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Employee employee = entityManager.find(Employee.class, 1);
 
-
-        EntityTransaction transaction = entityManager.getTransaction();
+        EntityTransaction transaction =  entityManager.getTransaction();
         transaction.begin();
-        entityManager.remove(employee);
+        entityManager.persist(employee);
+        entityManager.persist(employee2);
+        entityManager.persist(card1);
+        entityManager.persist(card2);
+
         transaction.commit();
         entityManager.close();
         entityManagerFactory.close();
-
-
-
-
-//
-//        Employee employee = new Employee();
-////        employee.setId(1);
-//        employee.setName("Foo Bar");
-//        employee.setSsn("123");
-//        employee.setDob(new Date());
-//        employee.setAge(20);
-//        employee.setType(EmployeeType.CONTRACTOR);
-//
-//        Employee employee2 = new Employee();
-////        employee2.setId(2);
-//        employee2.setName("Bar bazzzz");
-//        employee2.setSsn("456");
-//        employee2.setDob(new Date());
-//        employee2.setAge(30);
-//        employee2.setType(EmployeeType.FULL_TIME);
-//
-//
-//
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//
-//        EntityTransaction transaction =  entityManager.getTransaction();
-//        transaction.begin();
-//        entityManager.persist(employee);
-//        entityManager.persist(employee2);
-//
-//        transaction.commit();
-//        entityManager.close();
-//        entityManagerFactory.close();
 
     }
 }
