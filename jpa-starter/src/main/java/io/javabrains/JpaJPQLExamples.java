de366fa90086b86e52a175c7c49cb757305a72cf
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-12-26 14:43:18 -0500

    41.2 - between clause in JPQL

--- jpa-starter/src/main/java/io/javabrains/JpaJPQLExamples.java
+++ jpa-starter/src/main/java/io/javabrains/JpaJPQLExamples.java
@@ -11,7 +11,7 @@ public class JpaJPQLExamples {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        TypedQuery<Employee> query = entityManager.createQuery("select e from Employee e where e.name like '%Bar'", Employee.class);
+        TypedQuery<Employee> query = entityManager.createQuery("select e from Employee e where e.age between 15 and 25", Employee.class);
         List<Employee> resultList = query.getResultList();
         resultList.forEach(System.out::println);
 
